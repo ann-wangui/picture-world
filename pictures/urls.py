1030d34e046f164d9869de9f932286c7e41f1582
ann-wangui <wanguia822@gmail.com>
2022-05-30 00:50:16 +0300

    commit all changes

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import re_path
+from django.conf.urls import path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^search/', views.search_results, name='search'),
-    re_path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
+    path(r'^$', views.index, name='index'),
+    path(r'^search/', views.search_results, name='search'),
+    path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
